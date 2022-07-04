70379ddd2817a57b44d420ad18d89551d1f2fa2a
Obichukwu uchenna <uobichukwu@web.de>
2022-07-04 15:09:06 -0700

    python_hello_world_new

--- 0x00-python-hello_world/5-no_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    strings = ""
-    for i in my_string:
-        if i == "c" or i =="C":
-            continue
-        else:
-            strings += i
-    return strings
